b6cc3fa3d9e5eebd0bf2eedbf8d2953683f8e14e
harshb <harshb@metrumrg.com>
2020-12-11 20:22:52 -0500

    adding chance

--- chat/trigger.js
+++ chat/trigger.js
@@ -2,7 +2,7 @@ const Trigger = require('../models/trigger');
 const parseArgs = require('../utils/parseArgs');
 
 const trigger = async (message) => {
-	// if(Math.round(Math.random() * 9) !== 6) return;
+	if(Math.round(Math.random() * 9) !== 6) return;
 
 	const data = await Trigger.find({});
 	let result = data.find(text => message.content.includes(text.trigger))
