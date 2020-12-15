7368aa2351e794bfb0a28222568a7fe2db0cc649
harshb <harshb@metrumrg.com>
2020-12-14 20:47:40 -0500

    fixing trigger

--- chat/trigger.js
+++ chat/trigger.js
@@ -5,7 +5,8 @@ const trigger = async (message) => {
 	if(Math.round(Math.random() * 9) !== 6) return;
 
 	const data = await Trigger.find({});
-	let result = data.find(text => message.content.includes(text.trigger))
+	let msg  = message.content.toLowerCase();
+	let result = data.find(text => msg.includes(text.trigger))
 
 	if(typeof result !== 'undefined'){
 		let responses = result.responses
